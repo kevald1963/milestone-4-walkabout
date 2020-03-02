518213b9d62b9299ee83f1011720c6f83702150d
kevald1963 <kevin_alderson@yahoo.co.uk>
2020-03-02 17:05:50 +0000

    Started creating facility to edit an Organisation.

--- organisation/views.py
+++ organisation/views.py
@@ -6,3 +6,8 @@ def all_organisations(request):
     """ Get all the organisations from the database. """
     organisations = Organisation.objects.all()
     return render(request, "organisation.html", {"organisations": organisations})
+
+def edit_organisation(request):
+    """ Get the selected organisations from the database for editing. """
+    organisation = Organisation.objects.all()
+    return render(request, "edit_organisation.html", {"organisation": organisation})
