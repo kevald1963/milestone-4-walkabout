1878c789ad8e4cbdcdd4b7416d69cfdd1ab0eb29
kevald1963 <kevin_alderson@yahoo.co.uk>
2020-03-08 16:37:30 +0000

    Added is_parent flag to organisation/models.py. Added Meta class to same file to order by is_parent, so that parent organisation appears first in organisations.html.

--- organisation/models.py
+++ organisation/models.py
@@ -5,19 +5,23 @@ class Organisation(models.Model):
 
     objects = models.Manager()
     
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length=50)
     description = models.TextField()
-    logo_text = models.CharField(max_length = 15, blank=True)
-    glyphicon_name = models.CharField(max_length = 20, blank=True)
+    is_parent = models.BooleanField(verbose_name='Is parent organisation?', default=False)
+    logo_text = models.CharField(max_length=15, blank=True)
+    glyphicon_name = models.CharField(max_length=20, blank=True)
     image_path = models.TextField(blank=True)
-    image = models.ImageField(upload_to = 'images', blank=True)
-    image_caption = models.CharField(max_length = 70, blank=True)
-    address_1 = models.CharField(max_length = 40)
-    address_2 = models.CharField(max_length = 40)
-    address_3 = models.CharField(max_length = 40, blank=True)
-    address_4 = models.CharField(max_length = 40, blank=True)
-    post_code = models.CharField(max_length = 10)
-    email_address = models.CharField(max_length = 40)
+    image = models.ImageField(upload_to='images', blank=True)
+    image_caption = models.CharField(max_length=70, blank=True)
+    address_1 = models.CharField(max_length=40)
+    address_2 = models.CharField(max_length=40)
+    address_3 = models.CharField(max_length=40, blank=True)
+    address_4 = models.CharField(max_length=40, blank=True)
+    post_code = models.CharField(max_length=10)
+    email_address = models.CharField(max_length=40)
+
+    class Meta:
+        ordering = ['-is_parent']
 
     def __str__(self):
         return self.name
