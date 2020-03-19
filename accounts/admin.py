358c7645274a79cb8f3a2fa5a3abea9bc9d7569d
kevald1963 <kevin_alderson@yahoo.co.uk>
2020-03-19 15:16:56 +0000

    Created accounts/admin.py to limit Admin functionality for non-superusers. Changed base.html to removed stray characters appearing in navbar.

---
+++ accounts/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+# Unregister the provided model admin
+admin.site.unregister(User)
+
+# Register our own model admin, based on the default UserAdmin.
+# This extends the User model rather than replacing it.
+@admin.register(User)
+
+class CustomUserAdmin(UserAdmin):
+
+    # Disable last_login and date_joined fields. These should never be editable.
+    readonly_fields = [
+        'last_login',
+        'date_joined',
+    ]
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+
+        # Only allow superusers to modify username, is_active, etc.
+        if not is_superuser:
+            form.base_fields['username'].disabled = True
+            form.base_fields['is_active'].disabled = True
+        return form
