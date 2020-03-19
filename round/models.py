5a3524b38ee72440cb67fcb7af07ac4ee510696b
kevald1963 <kevin_alderson@yahoo.co.uk>
2020-03-19 17:32:34 +0000

    Created 'round' app (i.e. delivery round) and related models.

---
+++ round/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Round(models.Model):
+    objects = models.Manager()
+
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length=60, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Street(models.Model):
+    objects = models.Manager()
+
+    name = models.CharField(max_length=40)
+    comments = models.TextField(blank=True)
+    door_number_start = models.SmallIntegerField()
+    door_number_end = models.SmallIntegerField()
+    round = models.ForeignKey(Round, on_delete=models.CASCADE)
+    post_code = models.CharField(max_length=10, null=False, blank=True)
+
+    def __str__(self):
+        return self.name
