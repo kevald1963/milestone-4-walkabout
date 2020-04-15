7587fb3ad044c3fa489321e381dc3dd9d693f459
kevald1963 <kevin_alderson@yahoo.co.uk>
2020-04-15 19:38:23 +0100

    Fixed comparison bug in js function SetDisableAttr. Added Django message functionality to Cart processing.

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from product.models import Product
 from organisation.models import Subscription
+from django.contrib import messages
 
 
 # Create your views here.
@@ -21,6 +22,7 @@ def add_to_cart(request, id):
     print("cart = " + str(cart))
     quantity = int(request.POST.get('quantity[]'))
     print("quantity 1 = " + str(quantity))
+    message = ''
 
     # Check if Free subscription product already saved to database, as it can only be registered once per customer.
     # If it is, then don't add product to Cart.
@@ -31,15 +33,20 @@ def add_to_cart(request, id):
             # do nothing
             print("subscription = " + str(subscription))
             quantity = 0
+            messages.add_message(request, messages.INFO, 'Product not added to cart - you own this product already!')
         else:
             quantity = 1
 
     print("quantity 2 = " + str(quantity))
 
+    # If product is already in cart, update the quantity, otherwise add the product to the
+    # cart with the selected quantity.
     if id in cart:
         cart[id] = int(cart[id]) + quantity
+        messages.add_message(request, messages.INFO, 'Quantity for this product updated in cart.')
     else:
         cart[id] = cart.get(id, quantity)
+        messages.add_message(request, messages.INFO, 'Product added to cart.')
 
     request.session['cart'] = cart
     return redirect(reverse('all_products'))
