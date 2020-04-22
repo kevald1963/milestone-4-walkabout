f98a2710c7fccb16d087c706d0f505088b1ca964
kevald1963 <kevin_alderson@yahoo.co.uk>
2020-04-22 21:55:52 +0100

    Added discount percent, amount and discounted total to checkout.html. Discount not shown if purchase is not eligible. Got 'gift' glyphicon to show in navbar for Products menu as had been wrongly named.

--- product/views.py
+++ product/views.py
@@ -5,5 +5,5 @@ from .models import Product, Discount
 # Create your views here.
 def all_products(request):
     products = Product.objects.all().order_by('number')
-    discount = Discount.objects.get(code=1)
-    return render(request, "products.html", {"products": products, "discount": discount})
+    percent = Discount.objects.values_list('percent', flat=True).get(code=1)
+    return render(request, "products.html", {"products": products, "percent": percent})
