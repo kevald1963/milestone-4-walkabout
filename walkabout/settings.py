ab8ebf2e1b205a2cf1edc428a4c2f713a6102278
kevald1963 <kevin_alderson@yahoo.co.uk>
2020-04-29 11:21:25 +0100

    Added '127.0.0.1' to ALLOWED_HOSTS (must be needed now that Heroku has been added to the same).

--- walkabout/settings.py
+++ walkabout/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['walkabout-app.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'walkabout-app.herokuapp.com']
 
 # Application definition
 
