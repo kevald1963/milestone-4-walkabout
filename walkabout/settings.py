1e713e50b50db8820b9010a44a7dd4b2dcb66a87
kevald1963 <kevin_alderson@yahoo.co.uk>
2020-05-12 13:16:37 +0100

    DEBUG set to False to allow testing on Heroku.

--- walkabout/settings.py
+++ walkabout/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'walkabout-app.herokuapp.com']
 
