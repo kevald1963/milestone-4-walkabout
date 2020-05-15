c0734747e491225952356edc3de382c54948784d
kevald1963 <kevin_alderson@yahoo.co.uk>
2020-05-15 14:13:33 +0100

    Made accounts/admin.py comply with PEP8 formatting standards.

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
+
 # Unregister the provided model admin
 admin.site.unregister(User)
 
 # Register our own model admin, based on the default UserAdmin.
 # This extends the User model rather than replacing it.
-@admin.register(User)
 
+
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
 
     # Disable last_login and date_joined fields. These should never be editable.
